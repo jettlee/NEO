a2107f58f70425210ee046268dfa07eb2d315dee
jettlee <leebigyu@gmail.com>
2018-04-04 18:10:16 -0400

    long polling js

---
+++ src/pollServer.js
@@ -0,0 +1,25 @@
+// define long polling to server
+var isActive = true;
+$().ready(function(){
+    pollServer();
+});
+
+function pollServer(){
+    if (isActive) {
+        window.setTimeout(function(){
+            $.ajax({
+                url: "http://localhost:5000",
+                type: "GET",
+                success: function(result){
+                    //logic
+                    console.log("success");
+                    pollServer();
+                },
+                error: function(){
+                    console.log("error");
+                    pollServer();
+                }
+            });
+        }, 2500);
+    };
+};
