cc1d32b856e9c55684e1a2890e3d6e0a4aa8ff82
Nisha Kunhikrishnan <nishakk@Nishas-MBP.wv.cc.cmu.edu>
2018-04-16 17:15:25 -0400

    Hover and show info

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var http = require('http');
 
 var app = express();
 var server = http.Server(app);
-var io = socketIO(server);
+var io = socketIO(server, {transports: ['websocket']});
 
 
 //app.set("view engine", "ejs");
